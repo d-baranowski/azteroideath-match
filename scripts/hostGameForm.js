a1ac1a42bfebb078721f2c19a10d3e33d50794a9
Daniel Baranowski <daniel.baranowski@infinityworks.com>
2019-01-25 23:40:14 +0000

    Add simple match making

--- scripts/hostGameForm.js
+++ scripts/hostGameForm.js
@@ -1,87 +1,131 @@
 import {createScaledroneRoom} from "./createScaledroneRoom.js";
 import {createConnection} from "./createConnection.js";
+import {randomNames} from "./randomNames.js";
 
 function initialForm(formPlaceholder) {
     formPlaceholder.innerHTML =
         `<button id="btnHostGameSubmit">Start</button><label for="txtGameName">Game Name</label><div id="txtGameName" />`;
 }
 
+const beginGame = (openConnection, startGame) => {
+    openConnection.publish({type: "GAME_START"});
+
+    const game = startGame();
+
+    const playerTwoController = game.playerController(1);
+    const {
+        thrust,
+        releaseThrust,
+        left,
+        right,
+        releaseTurn,
+        shoot,
+        shootRelease
+    } = playerTwoController;
+
+    openConnection.subscribe('CONTROLLER', (message) => {
+        if (message.type === 'CONTROLLER') {
+            switch (message.action) {
+                case "thrust":
+                    thrust();
+                    break;
+                case "releaseThrust":
+                    releaseThrust();
+                    break;
+                case "left":
+                    left(message.mod);
+                    break;
+                case "right":
+                    right(message.mod);
+                    break;
+                case "releaseTurn":
+                    releaseTurn();
+                    break;
+                case "shoot":
+                    shoot();
+                    break;
+                case "shootRelease":
+                    shootRelease();
+                    break;
+            }
+        }
+    });
+
+
+    setInterval(() => {
+        const buffer = msgpack.encode(game.serialize());
+        openConnection.publish({type: "GAME_STATE_UPDATE", payload: buffer})
+    }, 20)
+};
+
+const displayGameCreatorForm = async () => {
+  return new Promise((resolve, reject) => {
+      const roomName = randomNames();
+
+      document.getElementById('menuContainer').innerHTML =
+          `<div class="elements">
+            <div class="neon">${roomName}</div>
+            <div class="gameForm">
+                <label for="gameDuration">Game Duration</label>
+                <input id="gameDuration" min="1" max="60" type="number">
+            </div>
+            <div id="btnCreateGame" class="neon button">Create Game</div>
+        </div>`;
+
+      document.getElementById('btnCreateGame').onclick = () => {
+          resolve({
+              roomName,
+              gameDuration: document.getElementById("gameDuration").getAttribute("value")
+          });
+      };
+  })
+};
+
+const displayAwaitingPlayersForm = (roomName) => {
+    document.getElementById('menuContainer').innerHTML =
+        `<div class="elements">
+            <div class="neon">${roomName}</div>
+            <div class="playerInfo">You are player one</div>
+            <div id="playerTwoPlaceholder" class="playerInfo">Player Two: Awaiting</div>
+            <div id="btnStartGame" class="neon button">Start Game</div>
+        </div>`;
+};
+
 export const hostGameForm = (formPlaceholder, startGame) => {
     initialForm(formPlaceholder);
 
     (async () => {
-        const rtcConnectionRoomName = crypto.getRandomValues(new Uint32Array(3)).join('');
-        document.getElementById('txtGameName').innerText = rtcConnectionRoomName;
+        const roomId = crypto.getRandomValues(new Uint32Array(3)).join('');
+        const {...options} = await displayGameCreatorForm();
+        displayAwaitingPlayersForm(options.roomName);
 
         const matchMaker = await createScaledroneRoom({
             channelId: 'tbrd6Bv7LyXeG8xX',
             roomName: 'azteroideath-matchmaker'
         });
-        matchMaker.subscribe('LIST_MATCHES', (message) => {
+
+        const stopListing = matchMaker.subscribe('LIST_MATCHES', (message) => {
             if (message.type === 'LIST_MATCHES') {
-                matchMaker.publish({type: 'MATCH_AVAILABLE', paylaod: {gameName: rtcConnectionRoomName}})
+                matchMaker.publish({type: 'MATCH_AVAILABLE', paylaod: {roomId, roomName: options.roomName, options}})
             }
         });
 
-        const webRtcConnectionPromise = createConnection('tbrd6Bv7LyXeG8xX', rtcConnectionRoomName);
+        const webRtcConnectionPromise = createConnection('tbrd6Bv7LyXeG8xX', roomId);
         let openConnection;
 
         webRtcConnectionPromise.then((con) => {
             openConnection = con;
 
-            const game = startGame();
-
-            const playerTwoController = game.playerController(1);
-            const {
-                thrust,
-                releaseThrust,
-                left,
-                right,
-                releaseTurn,
-                shoot,
-                shootRelease
-            } = playerTwoController;
-
-            openConnection.subscribe('CONTROLLER', (message) => {
-                if (message.type === 'CONTROLLER') {
-                    switch (message.action) {
-                        case "thrust":
-                            thrust();
-                            break;
-                        case "releaseThrust":
-                            releaseThrust();
-                            break;
-                        case "left":
-                            left(message.mod);
-                            break;
-                        case "right":
-                            right(message.mod);
-                            break;
-                        case "releaseTurn":
-                            releaseTurn();
-                            break;
-                        case "shoot":
-                            shoot();
-                            break;
-                        case "shootRelease":
-                            shootRelease();
-                            break;
-                    }
+            openConnection.subscribe('PLAYER_JOINED', (msg) => {
+                if (msg.type === 'PLAYER_JOINED') {
+                    document.getElementById('playerTwoPlaceholder').innerText = `Player Two Joined`
                 }
             });
 
-
-            setInterval(() => {
-                const buffer = msgpack.encode(game.serialize());
-                openConnection.publish({type: "GAME_STATE_UPDATE", payload: buffer})
-            }, 20)
+            document.getElementById('btnStartGame').onclick = () => {
+                beginGame(openConnection, startGame);
+                stopListing();
+            };
         });
-
-        formPlaceholder.innerHTML = `<button id="btnCloseGame">Close</button>`;
-        document.getElementById('btnCloseGame').addEventListener('click', () => {
-            matchMaker.close();
-            openConnection && openConnection.close();
-            initialForm();
-        })
     })();
 };
--- scripts/joinGameForm.js
+++ scripts/joinGameForm.js
@@ -1,46 +1,85 @@
 import {createScaledroneRoom} from "./createScaledroneRoom.js";
 import {createConnection} from "./createConnection.js";
 
-export const joinGameForm = (formPlaceholder, startGame) => {
-    document.getElementById('btnJoinGame').addEventListener('click', async () => {
-        const matchMaker = await createScaledroneRoom({channelId: 'tbrd6Bv7LyXeG8xX', roomName: 'azteroideath-matchmaker'});
-        matchMaker.subscribe('MATCH_AVAILABLE', (message) => {
-            if (message.type === 'MATCH_AVAILABLE') {
-                const webRtcConnectionPromise = createConnection('tbrd6Bv7LyXeG8xX', message.paylaod.gameName);
-                let openConnection;
-
-                webRtcConnectionPromise.then((con) => {
-                    openConnection = con;
-
-                    const makeRemoteController = () => ({
-                        thrust: () => { openConnection.publish({type: "CONTROLLER", action: "thrust"}); },
-                        releaseThrust: () => { openConnection.publish({type: "CONTROLLER", action: "releaseThrust"}); },
-                        left: (mod) => { openConnection.publish({type: "CONTROLLER", action: "left", mod}); },
-                        right: (mod) => { openConnection.publish({type: "CONTROLLER", action: "right", mod}); },
-                        releaseTurn: () => { openConnection.publish({type: "CONTROLLER", action: "releaseTurn"}); },
-                        shoot: () => { openConnection.publish({type: "CONTROLLER", action: "shoot"}); },
-                        shootRelease: () => { openConnection.publish({type: "CONTROLLER", action: "shootRelease"}); },
-                    });
-
-                    const game = startGame(false, makeRemoteController);
-                    game.followPlayer = 1;
-
-                    openConnection.subscribe("GAME_STATE_UPDATE", (message) => {
-                        if (message.type === "GAME_STATE_UPDATE") {
-                            game.setState(message.payload.data);
-                        }
-                    })
-
-                });
-
-                formPlaceholder.innerHTML = `<button id="btnCloseGame">Close</button>`;
-                document.getElementById('btnCloseGame').addEventListener('click', () => {
-                    matchMaker.close();
-                    openConnection && openConnection.close();
-                })
-            }
-        });
-
-        matchMaker.publish({type: "LIST_MATCHES"})
+const startRemoteGame = (openConnection, startGame) => {
+    const makeRemoteController = () => ({
+        thrust: () => { openConnection.publish({type: "CONTROLLER", action: "thrust"}); },
+        releaseThrust: () => { openConnection.publish({type: "CONTROLLER", action: "releaseThrust"}); },
+        left: (mod) => { openConnection.publish({type: "CONTROLLER", action: "left", mod}); },
+        right: (mod) => { openConnection.publish({type: "CONTROLLER", action: "right", mod}); },
+        releaseTurn: () => { openConnection.publish({type: "CONTROLLER", action: "releaseTurn"}); },
+        shoot: () => { openConnection.publish({type: "CONTROLLER", action: "shoot"}); },
+        shootRelease: () => { openConnection.publish({type: "CONTROLLER", action: "shootRelease"}); },
     });
+
+    const game = startGame(false, makeRemoteController);
+    game.followPlayer = 1;
+
+    openConnection.subscribe("GAME_STATE_UPDATE", (message) => {
+        if (message.type === "GAME_STATE_UPDATE") {
+            game.setState(message.payload.data);
+        }
+    })
+};
+
+
+const joinRoom = (roomId, startGame) => {
+    const webRtcConnectionPromise = createConnection('tbrd6Bv7LyXeG8xX', roomId);
+    webRtcConnectionPromise.then((connection) => {
+        connection.publish({type: 'PLAYER_JOINED'});
+        const unsubscribe = connection.subscribe("GAME_START", (msg) => {
+            if (msg.type === "GAME_START") {
+                startRemoteGame(connection, startGame);
+                unsubscribe();
+            }
+        })
+    });
+};
+
+const renderAwailableMatches = (availableMatches, onElementPress) => {
+    document.getElementById('available-placeholder').innerHTML = Object.values(availableMatches).reduce((accumulator, value) => {
+        console.log(value);
+        return accumulator + `<div class="neon button" data-roomId="${value.roomId}">${value.roomName}</div>`
+    }, "");
+
+    document.querySelectorAll('div[data-roomId]').forEach(elem => {
+        elem.addEventListener('click', onElementPress);
+    })
+};
+
+export let availableMatches = {};
+
+let intervalIds = [];
+
+export const joinGameForm = async (formPlaceholder, startGame) => {
+    document.getElementById('menuContainer').innerHTML = `
+        <div class="elements">
+            <div class="neon">Available Matches</div>
+            <div id="available-placeholder"></div>
+        </div>`;
+
+    const matchMaker = await createScaledroneRoom({channelId: 'tbrd6Bv7LyXeG8xX', roomName: 'azteroideath-matchmaker'});
+    const unsubscribe = matchMaker.subscribe('MATCH_AVAILABLE', (message) => {
+        if (message.type === 'MATCH_AVAILABLE') {
+            availableMatches[message.paylaod.roomId] = message.paylaod;
+            renderAwailableMatches(availableMatches, (event) => {
+                const roomId = event.target.getAttribute('data-roomId');
+                joinRoom(roomId, startGame);
+                unsubscribe();
+                intervalIds.forEach((intervalId) => clearInterval(intervalId));
+                document.getElementById('menuContainer').innerHTML = `
+                    <div class="elements">
+                        <div class="neon">Waiting for Player One</div>
+                    </div>`;
+            });
+        }
+    });
+
+    intervalIds.push(setInterval(() => {
+        matchMaker.publish({type: "LIST_MATCHES"})
+    }, 2 * 1000));
+
+    intervalIds.push(setInterval(() => {
+        availableMatches = {};
+    }, 7 * 1000));
 };
